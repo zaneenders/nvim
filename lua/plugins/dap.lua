b6403db366e094e0b816b2efacf211f3e3c25305
Zane Enders <zane@Zanes-Mac-Studio.local>
2025-05-24 12:37:09 -0600

    Fix debug symbols

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,6 +5,17 @@ return {
       "theHamsta/nvim-dap-virtual-text",
     },
     config = function()
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+      for name, sign in pairs(LazyVim.config.icons.dap) do
+        sign = type(sign) == "table" and sign or { sign }
+        print(sign[1])
+        vim.fn.sign_define(
+          "Dap" .. name,
+          { texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3], text = sign[1] }
+        )
+      end
+
       local dap = require("dap")
 
       local lldb_dap_path = vim.fn.trim(vim.fn.system("xcrun -f lldb-dap"))
