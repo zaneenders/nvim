eec6a143d6dd5af4b01631c6792074c365bbf0dc
zane <39070793+zaneenders@users.noreply.github.com>
2026-02-03 20:24:59 -0700

    Add scribe commands

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zane NVIM Config
+
+Welcome to my [Neovim](https://neovim.io/) config building on top of 
+[LazyVim](https://www.lazyvim.org/).
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,16 +2,31 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Unbind <leader><leader> (redundant with <leader>ff)
-vim.keymap.del("n", "<leader><leader>")
+-- Load scribe functionality
+local scribe = require("config.scribe")
+
+-- Unbind <leader><leader> after LazyVim loads (redundant with <leader>ff)
+-- Use defer to ensure we run after all lazy-loaded plugins
+vim.defer_fn(function()
+  -- Check if the mapping exists and what it maps to
+  local mapping = vim.fn.maparg("<leader><leader>", "n")
+  if mapping ~= "" then
+    vim.keymap.del("n", "<leader><leader>")
+  end
+  -- Ensure no mapping exists, then set up which-key properly
+  local ok, wk = pcall(require, "which-key")
+  if ok then
+    wk.add({
+      { "<leader><leader>", group = "scribe" },
+    })
+  end
+end, 100)
 
 -- Scribe keybindings using <leader><leader> prefix
 vim.keymap.set("n", "<leader><leader>m", function()
-  vim.fn.system("scribe markdown")
-  vim.notify("Scribe: markdown", vim.log.levels.INFO)
+  scribe.run("markdown", "markdown")
 end, { desc = "Scribe Markdown" })
 
 vim.keymap.set("n", "<leader><leader>l", function()
-  vim.fn.system("scribe log")
-  vim.notify("Scribe: log", vim.log.levels.INFO)
+  scribe.run("log", "log")
 end, { desc = "Scribe Log" })
