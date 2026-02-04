413543d957737dafa3fe9965a5b23868a51f6de3
zane <39070793+zaneenders@users.noreply.github.com>
2026-02-03 19:45:29 -0700

    Setup keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Unbind <leader><leader> (redundant with <leader>ff)
+vim.keymap.del("n", "<leader><leader>")
+
+-- Scribe keybindings using <leader><leader> prefix
+vim.keymap.set("n", "<leader><leader>m", function()
+  vim.fn.system("scribe markdown")
+  vim.notify("Scribe: markdown", vim.log.levels.INFO)
+end, { desc = "Scribe Markdown" })
+
+vim.keymap.set("n", "<leader><leader>l", function()
+  vim.fn.system("scribe log")
+  vim.notify("Scribe: log", vim.log.levels.INFO)
+end, { desc = "Scribe Log" })
