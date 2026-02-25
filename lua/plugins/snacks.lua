016d7437dd876f9374286a1580578dedd1f1b991
zane <39070793+zaneenders@users.noreply.github.com>
2026-02-24 23:42:15 -0700

    fix directory changing

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,6 +4,12 @@ return {
     picker = {
       hidden = true, -- for hidden files
       ignored = true, -- for .gitignore files
+      -- Disable auto root detection to prevent changing cwd to subdirectories
+      root = false,
+    },
+    terminal = {
+      -- Prevent terminal from changing directories - always use vim's initial cwd
+      cwd = vim.fn.getcwd(-1, -1),
     },
   },
 }
